4464e1aa6f61c6875ec4a2fd10e31189e1a155ae
Arttu mahlakaarto <arttu.mahlakaarto@gmail.com>
2019-10-12 05:37:11 +0300

    removed clutter

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
-var request = require('request');
-var geolib = require('geolib');
-var GeoJSON = require('geojson');
-var solver = require('node-tspsolver');
 var express = require('express');
 
+var vello = require('./src/api.js');
+var calc = require('./src/functions.js');
+var geoPoint = require('./src/classes.js').geoPointData;
 var app = express();
+var listOfCities = [];
+var seed = '';
 
 app.use(express.static('static'))
 
 
+app.get('/test',function(req,res,err){
+    vello.getCities(function(body){
+        api_callback(body,res,req)
+    });
+})
+
+function api_callback(body,res,req){
+    var cityResponse = body.cities;
+    for(city in cityResponse){
+        var cityEnt = cityResponse[city];
+        var tempCity = new geoPoint(cityEnt.name,cityEnt.position,city);
+        listOfCities.push(tempCity);
+    }
+    var sorted = calc.
+
+}
+
+
+
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+ })
